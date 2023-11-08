9c00ca9102b8ec9eeefa8a3f07f15f68ad793d92
Emil Bergström <100773593+EmilBergstrom12@users.noreply.github.com>
2023-11-08 17:33:10 +0100

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import { KameleoonUtils } from "@kameleoon/javascript-sdk";
 
 // -- Constants values
-export const siteCode = "dhqz9s1tb6";
+export const siteCode = "m0u4ie562p";
 export const featureKey = "bouncing_ball_demo";
-export const visitorCode = KameleoonUtils.getVisitorCode("www.example.com");
+export const visitorCode = KameleoonUtils.getVisitorCode("https://emilbergstrom12.github.io/bouncing-ball/");
